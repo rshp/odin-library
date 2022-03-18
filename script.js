3fb228794d77a5f0636f17c0a77777b5834c8b71
shpr <roman.shpr@gmail.com>
2022-03-18 20:04:43 +0200

    Add basic card append logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+const libraryContainer = document.querySelector('.library');
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -14,6 +15,28 @@ function Book(title, author, pages, read) {
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
 
-function addBookToLibrary() {
-	//stuff
+function addBookToLibrary(title, author, pages, read) {
+	myLibrary.push(new Book(title, author, pages, read));
 }
+
+function appendBookCard(targetHTML, book) {
+	let bookCard = document.querySelector('.blueprint-card').cloneNode('deep');
+	bookCard.classList.remove('blueprint-card');
+	bookCard.querySelector('.author').innerHTML = book.author;
+	bookCard.querySelector('.book-title').innerHTML = book.title;
+	targetHTML.insertBefore(bookCard, targetHTML.lastElementChild);
+	//event listeners
+}
+
+function removeBookCard(book) {
+	//remove card
+	//remove array entry
+}
+
+addBookToLibrary('Title1', 'Author1', 22, true);
+addBookToLibrary('Title2', 'Author3', 122, false);
+addBookToLibrary('Title10', 'Author4', 322, true);
+
+appendBookCard(libraryContainer, myLibrary[0]);
+appendBookCard(libraryContainer, myLibrary[1]);
+appendBookCard(libraryContainer, myLibrary[2]);
