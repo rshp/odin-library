bb604c4a9c145d6d8b0febf41360f7ef1a6d1b3f
shpr <roman.shpr@gmail.com>
2022-03-19 14:37:09 +0200

    Add book mark read logic and several default books

--- script.js
+++ script.js
@@ -5,18 +5,15 @@ function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.read = false;
-	this.info = function () {
-		return `${this.title} by ${this.author}, ${pages} pages,${
-			read ? ' was read' : ' not read yet'
-		}`;
-	};
+	this.read = read;
 }
 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+Book.prototype.toggleRead = function () {
+	this.read = !this.read;
+};
 
-function addBookToLibrary(title, author, pages, read) {
-	myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(book) {
+	appendBookCard(libraryContainer, book);
 }
 
 function appendBookCard(targetHTML, book) {
@@ -24,19 +21,53 @@ function appendBookCard(targetHTML, book) {
 	bookCard.classList.remove('blueprint-card');
 	bookCard.querySelector('.author').innerHTML = book.author;
 	bookCard.querySelector('.book-title').innerHTML = book.title;
+	bookCard.querySelector('.book-pages').innerHTML = `${book.pages} pages`;
+	if (book.read)
+		bookCard.querySelector('.card-mark-read').classList.add('book-read');
 	targetHTML.insertBefore(bookCard, targetHTML.lastElementChild);
-	//event listeners
+	bookCard.querySelector('.card-mark-read').addEventListener('click', (e) => {
+		toggleBookRead(bookCard, book);
+	});
+	bookCard.querySelector('.card-delete').addEventListener('click', (e) => {
+		removeBookCard(bookCard, book);
+	});
 }
 
-function removeBookCard(book) {
-	//remove card
-	//remove array entry
+function removeBookCard(bookCard, book) {
+	bookCard.remove();
 }
 
-addBookToLibrary('Title1', 'Author1', 22, true);
-addBookToLibrary('Title2', 'Author3', 122, false);
-addBookToLibrary('Title10', 'Author4', 322, true);
+function toggleBookRead(bookCard, book) {
+	book.toggleRead();
+	bookCard.querySelector('.card-mark-read').classList.toggle('book-read');
+}
 
-appendBookCard(libraryContainer, myLibrary[0]);
-appendBookCard(libraryContainer, myLibrary[1]);
-appendBookCard(libraryContainer, myLibrary[2]);
+addBookToLibrary(new Book('Animal Farm (1945)', 'George Orwell', 112, true));
+addBookToLibrary(
+	new Book(
+		'The Hound of the Baskervilles',
+		'Sir Arthur Conan Doyle',
+		122,
+		false
+	)
+);
+addBookToLibrary(
+	new Book('The Stranger', 'Albert Camus, tr. Matthew Ward', 123, true)
+);
+addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
+addBookToLibrary(
+	new Book(
+		'Some veery long book title. It keeps on going and going and going and going and going and going and going and going and going and here it ends',
+		'Pablo Diego José Francisco de Paula Juan Nepomuceno María de los Remedios Cipriano de la Santísima Trinidad Ruiz y Picasso',
+		1,
+		false
+	)
+);
+addBookToLibrary(new Book('A Single Man', 'Christopher Isherwood', 152, false));
+addBookToLibrary(new Book('Ice', 'Anna Kavan', 158, false));
+addBookToLibrary(
+	new Book('Bonjour Tristesse', 'Françoise Sagan, tr. Irene Ash', 160, true)
+);
+addBookToLibrary(
+	new Book('The Great Gatsby', 'F. Scott Fitzgerald', 189, false)
+);
