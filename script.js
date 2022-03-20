260e093dc58fba3fba69301ab77d52290eec0748
shpr <roman.shpr@gmail.com>
2022-03-20 14:11:59 +0200

    Begin refactoring library functions into separate class

--- script.js
+++ script.js
@@ -1,6 +1,75 @@
 let myLibrary = [];
 const libraryContainer = document.querySelector('.library');
 
+class library {
+	constructor(libraryContainer) {
+		this.books = [];
+		this.libraryContainer = libraryContainer;
+		this.createAddBookButton();
+	}
+	addBook(book) {
+		if (!this.isInLibrary(book)) this.books.push(book);
+	}
+	createAddBookButton() {
+		const button = document.createElement('div');
+		button.classList.add('book-card', 'add-book-button');
+		this.libraryContainer.appendChild(button);
+	}
+
+	isInLibrary(book) {
+		return this.books.some((element) => element.title == book.title);
+	}
+	removeBook(book) {
+		this.books = this.books.filter(
+			(element) => element.title != book.title
+		);
+	}
+	selectBook(book) {
+		return this.books.find((element) => element.title == book.title);
+	}
+	toggleRead(book) {
+		this.selectBook(book).toggleRead();
+	}
+	createBookCard(book) {
+		const bookCard = document.createElement('div');
+		bookCard.classList.add('book-card');
+		bookCard.innerHTML = `
+		<p class="book-title"></p>
+		<div class="separator">
+			<div class="line"></div>
+			<span>by</span>
+		</div>
+		<p class="author"></p>
+		<div class="separator">
+			<div class="line"></div>
+			<p><br /></p>
+		</div>
+		<p class="book-pages"></p>
+		<div class="card-controls">
+			<div class="card-mark-read">
+				<img src="./icon/text-box-check-outline.svg" alt="" />
+			</div>
+			<div class="card-delete">
+				<img src="./icon/delete-outline.svg" alt="" />
+			</div>
+		</div>`;
+		bookCard.querySelector('.author').innerHTML = book.author;
+		bookCard.querySelector('.book-title').innerHTML = book.title;
+		bookCard.querySelector('.book-pages').innerHTML = `${book.pages} pages`;
+		if (book.read)
+			bookCard
+				.querySelector('.card-mark-read')
+				.classList.add('book-read');
+		return bookCard;
+	}
+	attachBookCard(bookCard) {
+		this.libraryContainer.insertBefore(
+			bookCard,
+			this.libraryContainer.lastElementChild
+		);
+	}
+}
+
 function Book(title, author, pages, read) {
 	this.title = title;
 	this.author = author;
@@ -12,25 +81,8 @@ Book.prototype.toggleRead = function () {
 	this.read = !this.read;
 };
 
-function addBookToLibrary(book) {
+function myLibrary.addBook(book) {
 	appendBookCard(libraryContainer, book);
-}
-
-function appendBookCard(targetHTML, book) {
-	let bookCard = document.querySelector('.blueprint-card').cloneNode('deep');
-	bookCard.classList.remove('blueprint-card');
-	bookCard.querySelector('.author').innerHTML = book.author;
-	bookCard.querySelector('.book-title').innerHTML = book.title;
-	bookCard.querySelector('.book-pages').innerHTML = `${book.pages} pages`;
-	if (book.read)
-		bookCard.querySelector('.card-mark-read').classList.add('book-read');
-	targetHTML.insertBefore(bookCard, targetHTML.lastElementChild);
-	bookCard.querySelector('.card-mark-read').addEventListener('click', () => {
-		toggleBookRead(bookCard, book);
-	});
-	bookCard.querySelector('.card-delete').addEventListener('click', () => {
-		removeBookCard(bookCard);
-	});
 }
 
 function removeBookCard(bookCard) {
@@ -105,7 +157,7 @@ function closeAddBookWindow(e) {
 		let bookAuthor = modalWrapper.querySelector('#author').value;
 		let bookPages = modalWrapper.querySelector('#pages').value;
 		let bookRead = modalWrapper.querySelector('#read').checked;
-		addBookToLibrary(new Book(bookTitle, bookAuthor, bookPages, bookRead));
+		myLibrary.addBook(new Book(bookTitle, bookAuthor, bookPages, bookRead));
 		modalWrapper.style.display = 'none';
 		[
 			modalWrapper.querySelector('#title').value,
@@ -117,8 +169,20 @@ function closeAddBookWindow(e) {
 }
 
 //Add some existing books to collection
-addBookToLibrary(new Book('Animal Farm (1945)', 'George Orwell', 112, true));
-addBookToLibrary(
+const libContainer = document.querySelector('.library');
+myLibrary = new library(libContainer);
+myLibrary.addBook(new Book('Animal Farm (1945)', 'George Orwell', 112, true));
+myLibrary.addBook(new Book('Eddd', 'Ezcf', 112, false));
+myLibrary.addBook(new Book('Vcz', 'Ozk', 112, true));
+myLibrary.addBook(new Book('Poeir', 'Mncb', 112, false));
+myLibrary.addBook(new Book('Rqobb', 'Lorem', 112, true));
+myLibrary.addBook(new Book('dbn/ass', 'xxWee', 112, true));
+myLibrary.removeBook(new Book('Rqobb', 'Lorem', 112, true));
+myLibrary.addBook(new Book('dbn/ass', 'xxWee', 112, true));
+let card = myLibrary.createBookCard(new Book('dbn/ass', 'xxWee', 112, true));
+myLibrary.attachBookCard(card);
+myLibrary.addBook(new Book('Animal Farm (1945)', 'George Orwell', 112, true));
+myLibrary.addBook(
 	new Book(
 		'The Hound of the Baskervilles',
 		'Sir Arthur Conan Doyle',
@@ -126,11 +190,11 @@ addBookToLibrary(
 		false
 	)
 );
-addBookToLibrary(
+myLibrary.addBook(
 	new Book('The Stranger', 'Albert Camus, tr. Matthew Ward', 123, true)
 );
-addBookToLibrary(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
-addBookToLibrary(
+myLibrary.addBook(new Book('The Hobbit', 'J.R.R. Tolkien', 295, false));
+myLibrary.addBook(
 	new Book(
 		'Some veery long book title. It keeps on going and going and going and going and going and going and going and going and going and here it ends',
 		'Pablo Diego José Francisco de Paula Juan Nepomuceno María de los Remedios Cipriano de la Santísima Trinidad Ruiz y Picasso',
@@ -138,11 +202,12 @@ addBookToLibrary(
 		false
 	)
 );
-addBookToLibrary(new Book('A Single Man', 'Christopher Isherwood', 152, false));
-addBookToLibrary(new Book('Ice', 'Anna Kavan', 158, false));
-addBookToLibrary(
+myLibrary.addBook(new Book('A Single Man', 'Christopher Isherwood', 152, false));
+myLibrary.addBook(new Book('Ice', 'Anna Kavan', 158, false));
+myLibrary.addBook(
 	new Book('Bonjour Tristesse', 'Françoise Sagan, tr. Irene Ash', 160, true)
 );
-addBookToLibrary(
+myLibrary.addBook(
 	new Book('The Great Gatsby', 'F. Scott Fitzgerald', 189, false)
 );
+//-----------------------------------------------------------------
